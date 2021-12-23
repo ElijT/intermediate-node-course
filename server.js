d4fe5aa4a87f78fc6e8d43f73a2897b75de838ca
Ubuntu <comptememl.eu>
2021-12-23 10:53:40 +0000

    add READ route

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const mongoose= require('mongoose');
 const bodyParser= require('body-parser');
 
 const User = require('./models/User');
-
-mongoose.connect('mongodb://clusterdb.ehjc5.mongodb.net/userData');
+const uri = "mongodb+srv://ElDen:totorh@clusterdb.ehjc5.mongodb.net/UserData?retryWrites=true&w=majority";
+mongoose.connect(uri);
 
 const port=8000;
 const app= express();
@@ -17,14 +17,46 @@ app.listen(port, ()=>{
 
 // CREATE
 app.post('/users',(req,res)=>{
-  // User.create()
+  User.create(
+    {
+      name:req.body.newData.name,
+      email:req.body.newData.email,
+      password:req.body.newData.password
+    },
+    (err,data)=>{
+    if (err){
+      res.json({success: false,message: err})
+    } else if (!data){
+      res.json({success: false,message: "Not Found"})
+    } else {
+      res.json({success: true,data: data})
+    }
+  })
 })
 
 app.route('/users/:id')
 // READ
 .get((req,res)=>{
-  // User.findById()
+  User.findById(req.params.id,(err,data) =>{
+	if (err){
+	  res.json({
+	    success: false,
+	    message: err
+	  })
+	} else if (!data){
+	  res.json({
+	    success: false,
+	    message: "Not found"
+	  })
+	} else {
+	  res.json({
+	    success: true,
+	    data: data
+	  })
+	}
+  })
 })
+
 // UPDATE
 .put((req,res)=>{
   // User.findByIdAndUpdate()
